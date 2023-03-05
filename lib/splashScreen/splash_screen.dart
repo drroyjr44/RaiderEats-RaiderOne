1cd5603e111642011456e6b1da7892c1bf4368be
Edwin Perez <Edwin.Perez@ttu.edu>
2023-03-05 00:19:35 -0600

    Replaced image on splash screen

--- lib/splashScreen/splash_screen.dart
+++ lib/splashScreen/splash_screen.dart
@@ -31,34 +31,34 @@ class _MySplashScreenState extends State<MySplashScreen>
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Container(
-        color: Colors.red,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset("images/splash.jpg"),
+        child: Container(
+          color: Colors.black,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset("images/Raider.jpg"),
 
-              const SizedBox(height: 10,),
+                const SizedBox(height: 10,),
 
-              const Padding(
-                padding: EdgeInsets.all(18.0),
-                child: Text(
-                  "Raider Eats(TTU)",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.black54,
-                    fontSize: 40,
-                    fontFamily: "Signatra",
-                    letterSpacing: 3,
+                const Padding(
+                  padding: EdgeInsets.all(18.0),
+                  child: Text(
+                    "Raider Eats",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 30,
+                      fontFamily: "Signatra",
+                      letterSpacing: 3,
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
+
         ),
-        
-      ),
     );
   }
 }
